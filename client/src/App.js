ec9d604550da0930ed79e1b84c23626c56094341
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-01-13 18:18:38 +0600

    create new post page

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import RegisterPage from "./pages/RegisterPage";
 import Post from "./Post";
 import {Route, Routes} from "react-router-dom"
 import { UserContextProvider } from "./UserContext";
+import CreatePost from "./pages/CreatePost";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
               <Route index element={<IndexPage />} /> 
               <Route path='/login' element={<LoginPage />} />
               <Route path='/register' element={<RegisterPage />} />
+              <Route path='/create' element={<CreatePost/>}/>
           </Route>
         </Routes>
       </UserContextProvider>
